cfb62870342dfdb1a0338ace9fb91dd9d03329f5
Patrick Arnold <patrick.a@geniusmonkey.com>
2020-01-12 13:27:34 -0700

    Update for autumn theme

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "-*-hack-medium-r-normal-*-12-*-*-*-*-*-*-*" };
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -16,10 +16,12 @@ static const char col_cyan[]        = "#005577";
 static const char col_yellow[]		= "#C18B32";
 static const char col_brown[]		= "#594929";
 static const char col_dark_brown[]	= "#1C1713";
+static const char col_red[]			= "#731b07";
+static const char col_dark_red[]    = "#3c1408";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_dark_brown, col_gray2 },
-	[SchemeSel]  = { col_gray3, col_brown,  col_yellow  },
+	[SchemeNorm] = { col_gray3, col_dark_red, col_gray2 },
+	[SchemeSel]  = { col_gray3, col_red,  col_yellow  },
 };
 
 /* tagging */
@@ -60,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *roficmd[] = { "rofi", "-combi-modi", "drun", "-show", "combi",  "-modi", "combi" };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "/usr/bin/alacritty" };
 static const char *lockcmd[]  = { "slock", NULL };
 
 static Key keys[] = {
