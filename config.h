52428ff3b16cd405d10318542b015308748b1c5f
Patrick Arnold <patrick.a@geniusmonkey.com>
2020-06-13 12:43:38 -0700

    updateded dwm

--- config.h
+++ config.h
@@ -1,27 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 6;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int snap      = 3;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "-*-hack-medium-r-normal-*-12-*-*-*-*-*-*-*" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "-*-hack-medium-r-normal-*-16-*-*-*-*-*-*-*" };
 static const char dmenufont[]       = "Hack:size=10";
+static const char col_gray0[]       = "#101010";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_yellow[]		= "#C18B32";
-static const char col_brown[]		= "#594929";
-static const char col_dark_brown[]	= "#1C1713";
-static const char col_red[]			= "#731b07";
+static const char col_yellow[]      = "#C18B32";
+static const char col_brown[]       = "#594929";
+static const char col_dark_brown[]  = "#1C1713";
+static const char col_red[]         = "#731b07";
 static const char col_dark_red[]    = "#3c1408";
+static const char col_dark_blue[]   = "#19354a";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_dark_red, col_gray2 },
-	[SchemeSel]  = { col_gray3, col_red,  col_yellow  },
+	/*               fg         bg         :border   */
+	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
+	[SchemeSel]  = { col_gray3, col_gray0, col_dark_blue  },
 };
 
 /* tagging */
